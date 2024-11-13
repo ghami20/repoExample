b4c782294612ccdc09984eafb4056d504280464b
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2024-11-13 18:22:33 -0300

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+function verMenu(){
+    var menu = document.getElementById("menu");
+
+    if (menu.style.display == "none") {
+        menu.style="display:block";
+    } else {
+        menu.style="display:none";
+
+    }
+
+
+}
